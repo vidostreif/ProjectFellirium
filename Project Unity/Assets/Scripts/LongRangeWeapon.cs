3d02c84eca3af4154f75c7ee2255ce2b8a69fcf8
vido <vido1@yandex.ru>
2016-10-01 19:53:44 +0400

    доработка стрел

--- Project Unity/Assets/Scripts/LongRangeWeapon.cs
+++ Project Unity/Assets/Scripts/LongRangeWeapon.cs
@@ -41,8 +41,24 @@ public class LongRangeWeapon : MonoBehaviour {
             
             //расчетный угол
             float agleT = AgleBalistic(distance, distance * Mathf.Sqrt(distance));
-            //Скорость цели если увеличиваем, то снижаем угол
-            float targetSpeed = (Mathf.Abs(targetRigidbody2D.velocity.x*1.7f)) / distance / Mathf.Sqrt(newBulletRigidbody.gravityScale) ;
+
+            //Скорость цели 
+            //если увеличиваем, то снижаем угол
+            float targetSpeed = 0;
+            if (distance > 10)
+            {
+                //float targetSpeed = Mathf.Sqrt((Mathf.Abs(targetRigidbody2D.velocity.x - targetRigidbody2D.velocity.y/2)) * ((distance - (target.transform.position.y - thisTransform.position.y)) / 1000f) / (newBulletRigidbody.gravityScale));
+                //targetSpeed = Mathf.Sqrt((Mathf.Abs(targetRigidbody2D.velocity.x) / distance / 1.9f / (newBulletRigidbody.gravityScale)));
+                //float targetSpeed = ((Mathf.Abs(targetRigidbody2D.velocity.x))  * 2f / Mathf.Sqrt(newBulletRigidbody.gravityScale) / distance);
+                //float targetSpeed = Mathf.Sqrt((Mathf.Abs(targetRigidbody2D.velocity.x)) /  (newBulletRigidbody.gravityScale))/ distance * 3;
+                //targetSpeed = Mathf.Sqrt((Mathf.Abs(targetRigidbody2D.velocity.x) / distance/ (newBulletRigidbody.gravityScale) ));
+                //targetSpeed = (Mathf.Abs(targetRigidbody2D.velocity.x) / distance * 1.5f / (newBulletRigidbody.gravityScale));
+                targetSpeed = (Mathf.Abs(targetRigidbody2D.velocity.x - targetRigidbody2D.velocity.y) / distance * 1.5f / Mathf.Sqrt(newBulletRigidbody.gravityScale));
+
+                //targetSpeed = Mathf.Sqrt(Mathf.Abs(targetRigidbody2D.velocity.x) * (distance)/1700 / (newBulletRigidbody.gravityScale));
+
+            }
+
             //корректировка на угол минус скорость цели
             vShotDirection = new Vector3(vShotDirection.x, vShotDirection.y + (agleT / 90) - targetSpeed, vShotDirection.z);
                         
@@ -54,8 +70,8 @@ public class LongRangeWeapon : MonoBehaviour {
             newBulletBulletScript.ToTurn(vShotDirection);
             
             //Стреляем
-            attackForce = vShotDirection * Mathf.Sqrt(distance) * newBulletRigidbody.mass * Mathf.Sqrt(newBulletRigidbody.gravityScale) * 110;
-            newBulletRigidbody.AddForceAtPosition(attackForce, new Vector2(0, 0));
+            attackForce = vShotDirection * Mathf.Sqrt(distance) * newBulletRigidbody.mass * Mathf.Sqrt(newBulletRigidbody.gravityScale) * Random.Range(108f, 111f);
+            newBulletRigidbody.AddForceAtPosition(new Vector3(attackForce.x, attackForce.y, attackForce.z), new Vector2(0, 0));
             //newBulletRigidbody.AddForceAtPosition(vShotDirection * Mathf.Sqrt(distance) * Random.Range(10.7f, 10.95f), new Vector2(0,0));
 
             //указываем время последнего выстрела
